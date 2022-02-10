72c01428e4c2752e2708478fe297fdb8b31c5564
Rajat Menon <Rajat.Menon@aqura.com.au>
2022-02-10 10:22:48 +0800

    Initial Repo

---
+++ src/data.js
@@ -0,0 +1,58 @@
+export default {
+  "name": "Data",
+      "children": [
+        {
+          "name": "Camp Admin",
+          "children": [
+            {
+              "name": "Devices",
+              "children": [
+                {
+                  "name": "147dda3e755c", "minSize": 10, "size": 3000
+                },
+                {
+                  "name": "ccf9e4d6f709", "minSize": 10, "size": 3000
+                },
+                {
+                  "name": "ccf9e4d2d8ea", "minSize": 10, "size": 3000
+                },
+                {
+                  "name": "b0be834f6026", "minSize": 10, "size": 3000
+                }
+              ]
+            }
+          ]
+        },
+        {
+          "name": "Hitesh Bechar",
+          "children": [
+            {
+              "name": "Devices",
+              "children": [
+                {
+                  "name": "6a9eea3fdc09", "minSize": 10, "size": 3000
+                },
+                {
+                  "name": "103d0a0f2448", "minSize": 10, "size": 3000
+                },
+                {
+                  "name": "2eec1cf9681c", "minSize": 10, "size": 3000
+                },
+                {
+                  "name": "ee5170061046", "minSize": 10, "size": 3000
+                },
+                {
+                  "name": "4aa81ff983bc", "minSize": 10, "size": 3000
+                },
+                {
+                  "name": "0e847d8aa253", "minSize": 10, "size": 3000
+                },
+                {
+                  "name": "3ec5534e4a39", "minSize": 10, "size": 3000
+                }
+              ]
+            }
+          ]
+        }
+      ]
+    }
