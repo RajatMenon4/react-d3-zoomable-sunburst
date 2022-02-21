88ea1166c140c3eb403740e391b852520bf43258
Rajat Menon <Rajat.Menon@aqura.com.au>
2022-02-21 10:46:52 +0800

    Updates

---
+++ precache-manifest.5a17c626952e79ca3b87b6ae3d5e390c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dca3f352e0bc96e67ee7d5927d3ba5f9",
+    "url": "/react-d3-zoomable-sunburst/index.html"
+  },
+  {
+    "revision": "aa9fe58d2f54672f7997",
+    "url": "/react-d3-zoomable-sunburst/static/css/main.523f09c5.chunk.css"
+  },
+  {
+    "revision": "27ad319f769d45c58c37",
+    "url": "/react-d3-zoomable-sunburst/static/js/2.df20b847.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-d3-zoomable-sunburst/static/js/2.df20b847.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa9fe58d2f54672f7997",
+    "url": "/react-d3-zoomable-sunburst/static/js/main.8fe9dc32.chunk.js"
+  },
+  {
+    "revision": "ecdbb12a2e38b0ce99b2",
+    "url": "/react-d3-zoomable-sunburst/static/js/runtime-main.14a9224c.js"
+  }
+]);
